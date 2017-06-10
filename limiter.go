6b837f5fd4961699dd0ad0a42aca5a236db0a212
Haim Grosman <korovkin@gmail.com>
2017-06-10 15:50:53 -0700

    adding ExecuteExt

--- limiter.go
+++ limiter.go
@@ -53,6 +53,24 @@ func (c *ConcurrencyLimiter) Execute(job func()) int {
 	return ticket
 }
 
+// if num of go routines allocated by this instance is < limit
+// launch a new go routine to execut job
+// else wait until a go routine becomes available
+func (c *ConcurrencyLimiter) ExecuteWithTicket(job func(ticket int)) int {
+	ticket := <-c.tickets
+	atomic.AddInt32(&c.numInProgress, 1)
+	go func() {
+		defer func() {
+			c.tickets <- ticket
+			atomic.AddInt32(&c.numInProgress, -1)
+		}()
+
+		// run the job
+		job(ticket)
+	}()
+	return ticket
+}
+
 // wait until all the previously Executed jobs completed running
 //
 // IMPORTANT: calling the Wait function while keep calling Execute leads to
